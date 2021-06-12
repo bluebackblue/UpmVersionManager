a42daebb928814b820bf8761f833e1918be7a456
blueback <blueback28@gmail.com>
2021-06-13 05:08:09 +0900

    整理

---
+++ unity_UpmVersionManager/Assets/UPM/Samples~/NewProject/Editor/MenuItem.cs
@@ -0,0 +1,60 @@
+
+
+/** Samples.UpmVersionManager.NewProject.Editor
+*/
+namespace Samples.UpmVersionManager.NewProject.Editor
+{
+	/** MenuItem
+	*/
+	#if(UNITY_EDITOR)
+	public class MenuItem
+	{
+		/** MenuItem_NewProject
+
+			Assets
+				Editor
+					UpmVersionManagerSetting.cs
+
+				Samples
+					<<NewProject>>
+						000
+							xxxx
+							yyyy
+
+					Samples.asmdef
+
+				UPM
+					Editor
+						<<Owner>>
+							<<NewProject>>
+								Editor
+						<<Pwner>>.<<NewProject>.Editor.asmdef
+
+					RunTime
+						<<Owner>>
+							<<NewProject>>
+								Config.cs
+								DebugTool.cs
+								Version.cs
+						<<Pwner>>.<<NewProject>.asmdef
+
+					CHANGELOG.MD
+					LICENSE
+					package.json
+					README.md
+
+				csc.rsp
+				server.json
+				UpmVersionManagerWindow.uss
+				UpmVersionManagerWindow.uxml
+
+		*/
+		[UnityEditor.MenuItem("UpmVersionManager/NewProject")]
+		private static void MenuItem_NewProject()
+		{
+			
+		}
+	}
+	#endif
+}
+
