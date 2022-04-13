d4d5156fff22c817bfc02bc635215b68d759cad4
blueback <blueback28@gmail.com>
2022-04-13 21:02:39 +0900

    0.0.61

--- BlueBackVersionManager/Assets/UPM/Runtime/BlueBack/VersionManager/Version.cs
+++ BlueBackVersionManager/Assets/UPM/Runtime/BlueBack/VersionManager/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.VersionManager
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.60";
+		public const string packageversion = "0.0.61";
 
 		/** GetPackageVersion
 		*/
