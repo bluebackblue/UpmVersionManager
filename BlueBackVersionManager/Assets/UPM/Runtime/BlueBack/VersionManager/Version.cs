f3780c30888a6245e09a8e8f1c60106cc4b3df9b
blueback <blueback28@gmail.com>
2022-08-16 20:18:06 +0900

    整理

--- BlueBackVersionManager/Assets/UPM/Runtime/BlueBack/VersionManager/Version.cs
+++ BlueBackVersionManager/Assets/UPM/Runtime/BlueBack/VersionManager/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.VersionManager
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.77";
+		public const string packageversion = "0.0.78";
 
 		/** GetPackageVersion
 		*/
